db7383742f55168af2abf6e386cd334fa0525265
James Reichert <jdogjames@gmail.com>
2020-01-27 20:19:07 -0500

    customizes to use sources

--- functions/index.js
+++ functions/index.js
@@ -1,87 +1,120 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-
-admin.initializeApp(functions.config().firebase);
-
-const firestore = admin.firestore();
-const settings = { timestampInSnapshots: true };
-firestore.settings(settings);
-
+admin.initializeApp();
+//const logging = require('@google-cloud/logging')();
 const stripe = require('stripe')(functions.config().stripe.token);
+const currency = functions.config().stripe.currency || 'USD';
 
-exports.addStripeSource = functions.firestore.document('cards/{userId}/tokens/{tokenId}')
-    .onCreate(async (tokenSnap, context) => {
-        var customer;
-        const data = tokenSnap.data();
-        if (data === null) {
-            return null
+// [START chargecustomer]
+// Charge the Stripe customer whenever an amount is created in Cloud Firestore
+exports.createStripeCharge = functions.firestore.document('stripe_customers/{userId}/charges/{id}').onCreate(async (snap, context) => {
+    const val = snap.data();
+    try {
+        // Look up the Stripe customer id written in createStripeCustomer
+        const snapshot = await admin.firestore().collection(`stripe_customers`).doc(context.params.userId).get()
+        const snapval = snapshot.data();
+        const customer = snapval.customer_id
+        const destination_id = val.destination_id
+        // Create a charge using the pushId as the idempotency key
+        // protecting against double charges
+        const amount = val.amount;
+        const idempotencyKey = context.params.id;
+        const charge = { amount, currency, customer };
+        if (val.source !== null) {
+            charge.source = val.source;
         }
-        const token = data.tokenId;
-        const snapshot = await firestore.collection('users').doc(context.params.userId).get();
-        const customerId = snapshot.data().custId; // No custID in firestore Min 18:30
-        const customerEmail = snapshot.data().email;
-        if (customerId === 'new') {
+        const response = await stripe.charges.create(charge, {
+            idempotency_key: idempotencyKey, transfer_data: {
+                amount: charge.amount * 0.9,
+                destination: destination_id,
+            },
+        });
+        // If the result is successful, write it back to the database
+        return snap.ref.set(response, { merge: true });
+    } catch (error) {
+        // We want to capture errors and render them in a user-friendly way, while
+        // still logging an exception with StackDriver
+        console.log(error);
+        await snap.ref.set({ error: userFacingMessage(error) }, { merge: true });
+        return reportError(error, { user: context.params.userId });
+    }
+});
+// [END chargecustomer]]
 
-            customer = await stripe.customers.create({
-                email: customerEmail,
-                source: token
-            });
-            firestore.collection('users').doc(context.params.userId).update({
-                custId: customer.id
-            });
-            // customer = await stripe.customers.createSource(
-            //     customer.id,
-            //     {
-            //         source: token
-            //     });
+// When a user is created, register them with Stripe
+exports.createStripeCustomer = functions.auth.user().onCreate(async (user) => {
+    const customer = await stripe.customers.create({ email: user.email });
+    return admin.firestore().collection('stripe_customers').doc(user.uid).set({ customer_id: customer.id });
+});
 
-        }
-        else {
-            customer = await stripe.customers.retrieve(customerId);
-            // customer.sources.create({
-            //     card: token
-            // });
-            console.log("Already exists!");
-        }
+// Add a payment source (card) for a user by writing a stripe payment source token to Cloud Firestore
+exports.addPaymentSource = functions.firestore.document('/stripe_customers/{userId}/tokens/{pushId}').onCreate(async (snap, context) => {
+    const source = snap.data();
+    const token = source.tokenId;
+    if (source === null) {
+        return null;
+    }
 
-        const source = customer.sources.data[0];
-        console.log(source);
-        return await firestore.collection('cards').doc(context.params.userId)
-            .collection('sources').doc(source.card.fingerprint).set(source);
-    })
+    try {
+        const snapshot = await admin.firestore().collection('stripe_customers').doc(context.params.userId).get();
+        const customer = snapshot.data().customer_id;
+        const response = await stripe.customers.createSource(customer, { source: token });
+        return admin.firestore().collection('stripe_customers').doc(context.params.userId).collection("sources").doc(response.fingerprint).set(response, { merge: true });
+    } catch (error) {
+        await snap.ref.set({ 'error': userFacingMessage(error) }, { merge: true });
+        return reportError(error, { user: context.params.userId });
+    }
+});
 
-// exports.chargeUser = functions.firestore.document('newtransaction')
-//     .onCreate(async (tokenSnap, context) => {
-//         var customer;
-//         const data = tokenSnap.data();
-//         if (data === null) {
-//             return null
-//         }
-//         const token = data.tokenId;
-//         const snapshot = await firestore.collection('users').doc(context.params.userId).get();
-//         const customerId = snapshot.data().custId; // No custID in firestore Min 18:30
-//         const customerEmail = snapshot.data().email;
-//         if (customerId === 'new') {
-//             customer = await stripe.customers.create({
-//                 email: customerEmail,
-//                 source: token
-//             });
-//             firestore.collection('users').doc(context.params.userId).update({
-//                 custId: customer.id
-//             });
-//         }
+// When a user deletes their account, clean up after them
+exports.cleanupUser = functions.auth.user().onDelete(async (user) => {
+    const snapshot = await admin.firestore().collection('stripe_customers').doc(user.uid).get();
+    const customer = snapshot.data();
+    await stripe.customers.del(customer.customer_id);
+    return admin.firestore().collection('stripe_customers').doc(user.uid).delete();
+});
 
-//         else {
-//             customer = await stripe.customers.retrieve(customerId);
-//             stripe.customers.createSource(customer.id, {
-//                 source: token,
-//             });
-//         }
+// To keep on top of errors, we should raise a verbose error report with Stackdriver rather
+// than simply relying on console.error. This will calculate users affected + send you email
+// alerts, if you've opted into receiving them.
+// [START reporterror]
+function reportError(err, context = {}) {
+    // This is the name of the StackDriver log stream that will receive the log
+    // entry. This name can be any valid log stream name, but must contain "err"
+    // in order for the error to be picked up by StackDriver Error Reporting.
+ //   const logName = 'errors';
 
-//         const customerSources = customer.sources.data;
-//         return customerSources.forEach(function (source) {
-//             console.log(source);
-//             firestore.collection('cards').doc(context.params.userId)
-//                 .collection('sources').doc(customerSource.card.fingerprint).set(source, { merge: true });
-//         });
-//     })
+    // https://cloud.google.com/logging/docs/api/ref_v2beta1/rest/v2beta1/MonitoredResource
+    const metadata = {
+        resource: {
+            type: 'cloud_function',
+            labels: { function_name: process.env.FUNCTION_NAME },
+        },
+    };
+
+    // https://cloud.google.com/error-reporting/reference/rest/v1beta1/ErrorEvent
+    const errorEvent = {
+        message: err.stack,
+        serviceContext: {
+            service: process.env.FUNCTION_NAME,
+            resourceType: 'cloud_function',
+        },
+        context: context,
+    };
+
+    // Write the error log entry
+    return new Promise((resolve, reject) => {
+        log.write(log.entry(metadata, errorEvent), (error) => {
+            if (error) {
+                return reject(error);
+            }
+            return resolve();
+        });
+    });
+}
+// [END reporterror]
+
+// Sanitize the error message for the user
+function userFacingMessage(error) {
+    return error.type ? error.message : 'An error occurred, developers have been alerted';
+}
